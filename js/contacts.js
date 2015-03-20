bdb60e016495c83ce0037ba8979923dfa3a1e607
eermol <eermolaev@simtechdev.com>
2015-03-20 11:42:35 +0300

    commit

--- js/contacts.js
+++ js/contacts.js
@@ -2,7 +2,7 @@ var map;
     DG.then(function () {
         map = DG.map('map', {
             "center": [54.3494538996953,48.5324266352685],
-            "zoom": 16
+            "zoom": 14
         });
         DG.marker([54.3494538996953,48.5324266352685]).addTo(map).bindPopup('Вы кликнули по мне!');
     });
